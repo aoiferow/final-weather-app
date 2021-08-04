2c84e11fa71d9062ab34a73f5101d38e316e3be1
aoiferow <72607771+aoiferow@users.noreply.github.com>
2021-08-03 20:05:25 -0400

    Css styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
+import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
-  return (
-    <div className="App">
-      Hello
+  return (<div className="App">
+    <div className="container">
+      <Weather defaultCity="Boston" />
     </div>
+    <footer>
+      This project was coded by <a href="https://github.com/aoiferow/react-weather-app" target="_blank" rel="noreferrer">Aoife Rowland</a>
+    </footer>
+  </div>
+
   );
 }
 
-export default App;
+
