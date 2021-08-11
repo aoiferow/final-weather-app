4ef2f951dd7fd537c5b80514e11bd2f7dbdc09e8
aoiferow <72607771+aoiferow@users.noreply.github.com>
2021-08-10 20:12:33 -0400

    Formatted date js

--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
+import FormattedDate from "./FormattedDate.js";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
 
     const [weatherData, setWeatherData] = useState({ ready: false });
 
     function handleResponse(response) {
-        console.log(response.data);
         setWeatherData({
             ready: true,
             temperature: response.data.main.temp,
@@ -18,15 +18,21 @@ export default function Weather() {
             humidity: response.data.main.humidity,
             description: response.data.weather[0].description,
             iconUrl: "http://openweathermap.org/img/wn/01d@2x.png",
+            date: new Date(response.data.dt * 1000)
         })
 
     }
 
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        // Search for city
+    }
 
     if (weatherData.ready) {
         return (
             <div className="Weather">
-                <form className="form-inline">
+                <form onSubmit={handleSubmit} className="form-inline">
                     <div className="row">
                         <div className="col-9">
                             <input type="search" className="form-control" placeholder="Enter a City..." autoFocus="on" />
@@ -41,7 +47,7 @@ export default function Weather() {
                 </h1>
                 <ul>
                     <li>
-                        Wednesday 12:00pm
+                        <FormattedDate date={weatherData.date} />
                     </li>
                     <li className="text-capitalize">
                         {weatherData.description}
@@ -91,9 +97,8 @@ export default function Weather() {
 
     else {
 
-        let city = "Boston";
         const apiKey = "aed21243ee272b8cf9bddb7df0466769";
-        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`
         axios.get(apiUrl).then(handleResponse);
 
         return "Loading"
