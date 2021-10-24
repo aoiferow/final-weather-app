695aa08ebd072b1871713556da94acd2dbf8e59c
aoiferow <72607771+aoiferow@users.noreply.github.com>
2021-10-24 10:01:24 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
       <Weather defaultCity="Boston" />
     </div>
     <footer>
-      This project was coded by <a href="https://github.com/aoiferow/react-weather-app" target="_blank" rel="noreferrer">Aoife Rowland</a>
+      This project was coded by <a href="https://www.linkedin.com/in/aoife-rowland-641883b2/" target="_blank" rel="noreferrer">Aoife Rowland</a> and is <a href="https://github.com/aoiferow/final-weather-app" target="_blank" rel="noreferrer">Open Sourced on Github</a> and hosted by <a href="https://app.netlify.com/sites/elastic-kilby-f45335/overview" target="_blank" rel="noreferrer">Netlify</a>
     </footer>
   </div>
 
